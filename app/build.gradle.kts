25b355f46271cebfa42cc7c7b61a5ec65470de57
Y.Y.Daniel <626986815@qq.com>
2026-06-11 00:23:53 +0800

    修改版本号

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -17,7 +17,7 @@ android {
         minSdk = 29
         targetSdk = 36
         versionCode = 20260611
-        versionName = "1.0.1"
+        versionName = "1.0.2"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
 
